4338307b482f051dc35b8690d6e69032799abbf4
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-10-16 12:55:08 +0530

    import Link from react-route-dom

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,38 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <>
-      <div className="home">
-        <h1>Home page</h1>
+      <div className=" flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <img
+          className="size-25 shrink-0"
+          src="https://cdn-icons-png.flaticon.com/256/7603/7603333.png"
+          alt="ChitChat Logo"
+        />
+        <div>
+          <div className="text-xl font-medium text-black dark:text-white">
+            <Link to={"/about"}>About Us </Link>
+          </div>
+          <p className="text-gray-500 dark:text-gray-400">
+            Here's the about page.
+          </p>
+        </div>
+      </div>
+
+      <div className=" flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <img
+          className="size-25 shrink-0"
+          src="https://cdn-icons-png.flaticon.com/256/7603/7603333.png"
+          alt="ChitChat Logo"
+        />
+        <div>
+          <div className="text-xl font-medium text-black dark:text-white">
+            <Link to={"/contact"}>Contact Us </Link>
+          </div>
+          <p className="text-gray-500 dark:text-gray-400">
+            Here's the Contact page.
+          </p>
+        </div>
       </div>
     </>
   );
