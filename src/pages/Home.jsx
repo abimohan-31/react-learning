89cb87c1afa11d2c9420213c02567a30ccf28794
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-10-16 12:56:44 +0530

    removed image tag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,6 @@ function Home() {
   return (
     <>
       <div className=" flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <img
-          className="size-25 shrink-0"
-          src="https://cdn-icons-png.flaticon.com/256/7603/7603333.png"
-          alt="ChitChat Logo"
-        />
         <div>
           <div className="text-xl font-medium text-black dark:text-white">
             <Link to={"/about"}>About Us </Link>
@@ -20,11 +15,6 @@ function Home() {
       </div>
 
       <div className=" flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <img
-          className="size-25 shrink-0"
-          src="https://cdn-icons-png.flaticon.com/256/7603/7603333.png"
-          alt="ChitChat Logo"
-        />
         <div>
           <div className="text-xl font-medium text-black dark:text-white">
             <Link to={"/contact"}>Contact Us </Link>
