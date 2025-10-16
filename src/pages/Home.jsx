650cebd399389e39565becfe06326e856727b052
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-10-16 14:28:18 +0530

    Margin added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
-      <div className=" flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+      <div className="my-3 mx-2 flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
         <div>
           <div className="text-xl font-medium text-black dark:text-white">
             <Link to={"/about"}>About Us </Link>
@@ -13,8 +13,7 @@ function Home() {
           </p>
         </div>
       </div>
-
-      <div className=" flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+      <div className="my-3 mx-2 flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
         <div>
           <div className="text-xl font-medium text-black dark:text-white">
             <Link to={"/contact"}>Contact Us </Link>
