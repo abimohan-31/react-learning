223a356dd5b8a3e1b29305685172b40a61a63153
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-10-27 16:16:29 +0530

    fetchData using axios

--- src/componensts/axios/FetchData.jsx
+++ src/componensts/axios/FetchData.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function FetchData() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get(
+        "https://express-mongo-connection-sigma.vercel.app/api/users"
+      );
+      const userData = response.data.users;
+      console.log(userData);
+      setUsers(userData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className="content-container">
@@ -20,13 +39,17 @@ function FetchData() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>lkn</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+          {users.map((user) => (
+           
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.isActive}</td>
+                <td>{user.createdAt}</td>
+                <td>{user.updatedAt}</td>
+              </tr>
+          
+          ))}
         </tbody>
       </table>
     </>
